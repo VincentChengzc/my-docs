11c8c07c5dd74730091a22f7b20c05f0f52d66be
VincentChengZc <250409437@qq.com>
2024-01-04 13:46:51 +0800

    feat: ✨引入插件 vitepress-auto-configure-nav-sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,32 +1,20 @@
-import AutoSidebar from 'vite-plugin-vitepress-auto-sidebar';
+import AutoConfigureNavSidebarPlugin from '@w3ctech-editorial-department/vitepress-auto-configure-nav-sidebar'
+const { nav, sidebar } = AutoConfigureNavSidebarPlugin({
+  collapsed: true,
+  showNavIcon: false,
+  singleLayerNav: true,
+  showSidebarIcon: true,
+  ignoreFolders: ['.vuepress'],
+})
 export default {
-  title: ".Cheng的文档小站", // 网页左上角标题
+  title: "达拉崩叭叭的文档小站", // 网页左上角标题
   description: "记录一些笔记.",
   titleTemplate: "余生很短", // 页签右标题
   base: "/vite-press/",
   lastUpdated: true, // 最后更新时间
   themeConfig: {
-    // 导航栏
-    nav: [
-      { text: "首页", link: "/" },
-      { text: "博客", link: "/blog/home" },
-      { text: "短文", link: "/essay/home" },
-      { text: "闲谈", link: "/chat/home" },
-    ],
-    // 侧边栏
-    sidebar: [
-      {
-        text: "文档",
-        items: [
-          { text: "git命令行备忘", link: "/blog/git命令行备忘" },
-          { text: "Getting Started", link: "/getting-started" },
-        ],
-      },
-      {
-        text: "留白",
-        items: [{ text: "暂无", link: "/暂无" }],
-      },
-    ],
+    nav,
+    sidebar,
     // 页脚
     footer: {
       message: "Released under the MIT License.",
@@ -44,9 +32,6 @@ export default {
   vite: {
     plugins: [
       // add plugin
-      AutoSidebar({
-        path:"./docs"
-      })
     ],
     server: {
        // 配置可以IP访问
