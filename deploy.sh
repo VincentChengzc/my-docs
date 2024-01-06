1dc7e8b440ee06acb4dfcb4c65f15fc45b5fe9a2
VincentChengZc <250409437@qq.com>
2024-01-06 15:42:01 +0800

    docs: 📝文档更新

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 提交所有代码到github
-git add .
-git commit -m 'docs: 📝文档更新'
-git push
 
 # 打包生成静态文件
 turbo docs:build
@@ -20,5 +16,11 @@ git commit -m 'docs: 📝文档更新'
 
 # 部署到 https://<username>.github.io/<repo>
 git push -f https://github.com/VincentChengzc/my-docs.git main:github-pages
+
+# 提交所有代码到github
+cd ../../../
+git add .
+git commit -m 'docs: 📝文档更新'
+git push
 
 
